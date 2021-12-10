226dde31d22ce1c0ad0df2f4a400c51330a96fb2
Madeleine <mahdieh.moghadasi@gmail.com>
2021-12-10 12:39:01 -0500

    Roman to Integer

---
+++ Roman_to_Integer.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {
+
+    const obj = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    let total = 0;
+    
+    for (i = 0; i < s.length; i++) {
+        
+        const current = obj[s[i]];
+        const next = obj[s[i+1]];
+        
+        if (current < next) {
+            total -= current
+            
+        } else {
+            total += current;
+        }
+            
+    }
+    return total;
+};
